949bd24b30077896ddb4aa021394aa0bf44004af
Романчук Богдан Володимирович <ipz244_rbv@student.ztu.edu.ua>
2026-04-28 17:16:56 +0300

    Add Visitor pattern for text extraction

--- task5/Program.cs
+++ task5/Program.cs
@@ -2,56 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
-public interface INodeState
+public interface IVisitor
 {
-    string RenderTag(string tagName, string classes, string innerHtml, ClosingType closingType);
+    void VisitElementNode(LightElementNode node);
+    void VisitTextNode(LightTextNode node);
 }
 
-public class VisibleState : INodeState
+public class TextExtractionVisitor : IVisitor
 {
-    public string RenderTag(string tagName, string classes, string innerHtml, ClosingType closingType)
+    public StringBuilder ExtractedText { get; } = new StringBuilder();
+
+    public void VisitElementNode(LightElementNode node)
     {
-        StringBuilder sb = new StringBuilder();
-        sb.Append("<").Append(tagName);
-
-        if (!string.IsNullOrEmpty(classes))
-        {
-            sb.Append(" class=\"").Append(classes).Append("\"");
-        }
-
-        sb.Append(">");
-
-        if (closingType == ClosingType.Paired)
-        {
-            sb.Append(innerHtml);
-            sb.Append("</").Append(tagName).Append(">");
-        }
-
-        return sb.ToString();
     }
-}
 
-public class HiddenState : INodeState
-{
-    public string RenderTag(string tagName, string classes, string innerHtml, ClosingType closingType)
+    public void VisitTextNode(LightTextNode node)
     {
-        StringBuilder sb = new StringBuilder();
-        sb.Append("<").Append(tagName);
-
-        if (!string.IsNullOrEmpty(classes))
-        {
-            sb.Append(" class=\"").Append(classes).Append("\"");
-        }
-
-        sb.Append(" style=\"display: none;\">");
-
-        if (closingType == ClosingType.Paired)
-        {
-            sb.Append(innerHtml);
-            sb.Append("</").Append(tagName).Append(">");
-        }
-
-        return sb.ToString();
+        ExtractedText.Append(node.OuterHTML).Append(" ");
     }
 }
 
@@ -71,6 +38,7 @@ public abstract class LightNode
 {
     public abstract string OuterHTML { get; }
     public abstract string InnerHTML { get; }
+    public abstract void Accept(IVisitor visitor);
 }
 
 public class LightTextNode : LightNode
@@ -84,6 +52,11 @@ public class LightTextNode : LightNode
 
     public override string OuterHTML => _text;
     public override string InnerHTML => _text;
+
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.VisitTextNode(this);
+    }
 }
 
 public class LightElementNode : LightNode
@@ -93,7 +66,6 @@ public class LightElementNode : LightNode
     private readonly ClosingType _closingType;
     private readonly List<string> _cssClasses;
     private readonly List<LightNode> _children;
-    private INodeState _state;
 
     public LightElementNode(string tagName, DisplayType displayType, ClosingType closingType)
     {
@@ -102,12 +74,6 @@ public class LightElementNode : LightNode
         _closingType = closingType;
         _cssClasses = new List<string>();
         _children = new List<LightNode>();
-        _state = new VisibleState();
-    }
-
-    public void SetState(INodeState state)
-    {
-        _state = state;
     }
 
     public void AddClass(string className)
@@ -139,8 +105,32 @@ public class LightElementNode : LightNode
     {
         get
         {
-            string classes = string.Join(" ", _cssClasses);
-            return _state.RenderTag(_tagName, classes, InnerHTML, _closingType);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<").Append(_tagName);
+
+            if (_cssClasses.Count > 0)
+            {
+                sb.Append(" class=\"").Append(string.Join(" ", _cssClasses)).Append("\"");
+            }
+
+            sb.Append(">");
+
+            if (_closingType == ClosingType.Paired)
+            {
+                sb.Append(InnerHTML);
+                sb.Append("</").Append(_tagName).Append(">");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.VisitElementNode(this);
+        foreach (var child in _children)
+        {
+            child.Accept(visitor);
         }
     }
 }
@@ -152,16 +142,22 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.UTF8;
         
-        LightElementNode alert = new LightElementNode("div", DisplayType.Block, ClosingType.Paired);
-        alert.AddClass("alert-box");
-        alert.AddChild(new LightTextNode("Важливе повідомлення!"));
+        LightElementNode div = new LightElementNode("div", DisplayType.Block, ClosingType.Paired);
+        LightElementNode h1 = new LightElementNode("h1", DisplayType.Block, ClosingType.Paired);
+        h1.AddChild(new LightTextNode("Екстракція"));
+        LightElementNode p = new LightElementNode("p", DisplayType.Block, ClosingType.Paired);
+        p.AddChild(new LightTextNode("чистого тексту."));
 
-        Console.WriteLine("Поточний стан: Видимий");
-        Console.WriteLine(alert.OuterHTML);
+        div.AddChild(h1);
+        div.AddChild(p);
 
-        alert.SetState(new HiddenState());
+        TextExtractionVisitor visitor = new TextExtractionVisitor();
+        div.Accept(visitor);
 
-        Console.WriteLine("\nПоточний стан: Прихований");
-        Console.WriteLine(alert.OuterHTML);
+        Console.WriteLine("Згенерований HTML:");
+        Console.WriteLine(div.OuterHTML);
+        
+        Console.WriteLine("\nВитягнутий текст через Visitor:");
+        Console.WriteLine(visitor.ExtractedText.ToString().Trim());
     }
 }
